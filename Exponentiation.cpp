aa32b7a885db454c623aeff84f64d52ec3d797b3
liumihan <32925782+Liumihan@users.noreply.github.com>
2018-04-27 13:54:51 +0800

    first try
    my first push

---
+++ Exponentiation.cpp
@@ -0,0 +1,18 @@
+#include "iostream"
+#include "string"
+using namespace std;
+
+
+int main()
+{
+	freopen("input.txt","r",stdin);
+	string s;
+	int n;
+
+	while (cin >> s >> n)
+	{
+		//cout << s*n << endl;
+	}
+	system("pause");
+	return 0;
+}
